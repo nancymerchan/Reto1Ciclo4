008a0c31849e9060b3be247461315ce2ffcfe3e8
unknown <nancy.merchan.mt@correo.usa.edu.co>
2021-11-22 16:48:10 -0500

    ret1Ci4

---
+++ src/main/java/Reto1_Ciclo4/Reto1/Reto1Application.java
@@ -0,0 +1,19 @@
+//Modelo
+//Interface
+//Repositorio
+//Servicio
+//Controlador
+
+package Reto1_Ciclo4.Reto1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto1Application.class, args);
+	}
+
+}
---
+++ src/test/java/Reto1_Ciclo4/Reto1/Reto1ApplicationTests.java
@@ -0,0 +1,13 @@
+package Reto1_Ciclo4.Reto1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reto1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
